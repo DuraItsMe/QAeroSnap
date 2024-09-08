210c1430e767a22e72cdb11205d9b5805855a0eb
Dura <stoyann.fraillon@gmail.com>
2024-09-08 16:55:37 +0200

    Setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='QAeroSnap',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'PyQt6',
+        'pywin32'
+    ],
+    description='Aero Snap Implementation for PyQt6',
+    author='Dura',
+    url='https://github.com/DuraItsMe/QAeroSnap',
+)
